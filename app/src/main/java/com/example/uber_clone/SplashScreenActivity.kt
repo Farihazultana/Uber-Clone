fac8cc97f3d95ed3aabd8584ba202f10729570fa
Fariha Sultana <farihasultana237@gmail.com>
2023-04-25 11:55:40 +0600

    Initial commit

---
+++ app/src/main/java/com/example/uber_clone/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uber_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
